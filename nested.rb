006629f97dbdc407f2fc0a4958f6c626b0e5bde0
Faraz Hanif <hfaraz745@gmail.com>
2018-04-19 13:48:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -2,11 +2,20 @@
 def hopper
 	programmer_hash =
  		{
-        	:known_for => "COBOL",
+        	:grace_hopper => {
+          :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
      }
-		 
+		 		hopper[:grace_hopper]
 
 end
 
